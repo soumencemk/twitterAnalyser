9dffdfe94fa66f5ffaa01ce8ae2e63cb4d947842
Soumen Karmakar <soumen.karmakar@ericsson.com>
2020-10-22 17:07:40 +0100

    Adding Local support

--- src/main/java/com/soumen/twitterstream/controller/Controller.java
+++ src/main/java/com/soumen/twitterstream/controller/Controller.java
@@ -5,8 +5,6 @@ import com.soumen.twitterstream.service.SearchRecorderService;
 import com.soumen.twitterstream.service.TwitterGrabberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,15 +27,15 @@ public class Controller {
     private final SearchRecorderService searchRecorderService;
 
     @GetMapping(value = "/tweets/{text}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<TweetFeed> streamOfTweets(@PathVariable String text, @AuthenticationPrincipal OAuth2User principal, ServerWebExchange webExchange) {
-        searchRecorderService.recordSearch(principal.getAttribute("name"), text,webExchange);
+    public Flux<TweetFeed> streamOfTweets(@PathVariable String text, ServerWebExchange webExchange) {
+        searchRecorderService.recordSearch("local", text,webExchange);
         return twitterGrabberService.grabTweets(text);
 
     }
 
     @GetMapping("/user")
-    public Mono<Map<String, Object>> user(@AuthenticationPrincipal OAuth2User principal) {
-        return Mono.just(Collections.singletonMap("name", principal.getAttribute("name")));
+    public Mono<Map<String, Object>> user() {
+        return Mono.just(Collections.singletonMap("name", "localUser"));
     }
 }
 
