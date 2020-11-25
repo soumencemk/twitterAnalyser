fc8075cad13bacdb3a4a7915b5982b44a930f5d4
Soumen Karmakar <soumen.karmakar@ericsson.com>
2020-11-25 14:42:01 +0000

    Only API

--- src/main/java/com/soumen/twitterstream/controller/Controller.java
+++ src/main/java/com/soumen/twitterstream/controller/Controller.java
@@ -1,21 +1,14 @@
 package com.soumen.twitterstream.controller;
 
 import com.soumen.twitterstream.model.TweetFeed;
-import com.soumen.twitterstream.service.SearchRecorderService;
 import com.soumen.twitterstream.service.TwitterGrabberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.util.Collections;
-import java.util.Map;
 
 /**
  * @author Soumen Karmakar
@@ -23,21 +16,15 @@ import java.util.Map;
  */
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/twitter-analyser")
 public class Controller {
 
     private final TwitterGrabberService twitterGrabberService;
-    private final SearchRecorderService searchRecorderService;
 
-    @GetMapping(value = "/tweets/{text}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<TweetFeed> streamOfTweets(@PathVariable String text, @AuthenticationPrincipal OAuth2User principal, ServerWebExchange webExchange) {
-        searchRecorderService.recordSearch(principal.getAttribute("name"), text, webExchange);
+    @GetMapping(value = "/tweets/search/{text}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<TweetFeed> streamOfTweets(@PathVariable String text) {
         return twitterGrabberService.grabTweets(text);
 
-    }
-
-    @GetMapping("/user")
-    public Mono<Map<String, Object>> user(@AuthenticationPrincipal OAuth2User principal) {
-        return Mono.just(Collections.singletonMap("name", principal.getAttribute("name")));
     }
 }
 
--- src/main/java/com/soumen/twitterstream/repo/SearchRepo.java
+++
@@ -1,6 +0,0 @@
-package com.soumen.twitterstream.repo;
-
-import com.soumen.twitterstream.model.Search;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SearchRepo extends MongoRepository<Search,Long> {}
