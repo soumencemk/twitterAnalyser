3e0efd8423a91f3c9e6c5c0fd6f533af67a97323
Soumen Karmakar <soumen.karmakar@ericsson.com>
2020-11-25 14:45:20 +0000

    Cors

--- src/main/java/com/soumen/twitterstream/controller/Controller.java
+++ src/main/java/com/soumen/twitterstream/controller/Controller.java
@@ -4,10 +4,7 @@ import com.soumen.twitterstream.model.TweetFeed;
 import com.soumen.twitterstream.service.TwitterGrabberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 /**
@@ -22,6 +19,7 @@ public class Controller {
     private final TwitterGrabberService twitterGrabberService;
 
     @GetMapping(value = "/tweets/search/{text}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @CrossOrigin
     public Flux<TweetFeed> streamOfTweets(@PathVariable String text) {
         return twitterGrabberService.grabTweets(text);
 
